3ce0a2ab5e887189629dcbc92aebb82d68e9c7ba
koyatakuma <k.takuma.1.14@gmail.com>
2021-03-27 12:07:42 +0900

    新規作成

--- app/customer.php
+++ app/customer.php
@@ -4,13 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class customer extends Model
+class Customer extends Model
 {
     protected $dates = ['birthday','created_at','updated_at'];
 
     public function pref()
     {
 
-        return $this->belongsTo('App\pref');
+        return $this->belongsTo('App\Pref');
     }
 }
